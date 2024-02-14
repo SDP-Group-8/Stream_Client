9df8ae6bfe25590c79842db1d0e6d551e60756e1
AlexWebb03 <alexanderwebb03@gmail.com>
2024-02-14 16:41:49 +0000

    add files

---
+++ rtc.js
@@ -0,0 +1,123 @@
+var peer = null;
+let sdp = "";
+fetch("http://172.20.167.248:8000/request-offer",
+{
+    method: "get",
+    headers: {
+        'Content-Type': 'text/plain'
+    }
+})
+.then(response=> response.json())
+.then(data => {
+    createPeer(data.offer)
+});
+function createPeer (offerSDP) {
+    let config = {
+        iceServers: [
+            { urls: 'stun:stun.l.google.com:19302' },
+            { urls: 'stun:stun2.l.google.com:19302' }
+        ]
+    };
+    
+    peer = new RTCPeerConnection(config);
+    
+    captureCamera(offerSDP);
+}
+
+async function showDevices() {    
+    let devices = (await navigator.mediaDevices.enumerateDevices()).filter(i=> i.kind == 'videoinput')
+    console.log(devices)
+}
+
+function captureCamera (sdpOffer) {
+    let constraints = {
+        audio: false,
+        video: true
+    }; 
+
+    navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
+        stream.getTracks().forEach(function(track) {
+
+            applyContraints(track);
+
+            peer.addTrack(track, stream);            
+        });
+        return createAnswer(sdpOffer);
+    }, function(err) {
+        alert('Could not acquire media: ' + err);
+    });
+}
+
+
+async function createAnswer (sdp) {
+    let offer = new RTCSessionDescription({sdp: sdp, type: 'offer'});
+    await peer.setRemoteDescription(offer);
+    let answer = await peer.createAnswer();
+
+    await peer.setLocalDescription(answer);
+    await new Promise(resolve => {
+        if (peer.iceGatheringState === 'complete') {
+            resolve();
+        } else {
+            const checkState = () => {
+                if (peer.iceGatheringState === 'complete') {
+                    peer.removeEventListener('icegatheringstatechange', checkState);
+                    resolve();
+                }
+            };
+            peer.addEventListener('icegatheringstatechange', checkState);
+        }
+    });
+
+    sendAnswerToBrowser(peer.localDescription.sdp);
+}
+
+function applyContraints (videoTrack) {
+    if (videoTrack) {
+    
+        const videoConstraints = {
+            width: { min: 320, max: 1280 },
+            height: { min: 240,  max: 720 },
+            frameRate: {min: 15,  max: 30 }
+        };
+    
+        // Apply video track constraints
+        videoTrack.applyConstraints(videoConstraints)
+            .then(() => {
+                console.log("Video track constraints applied successfully");
+            })
+            .catch((error) => {
+                console.error("Error applying video track constraints:", error);
+                setTimeout(() => {
+                    applyContraints();
+                }, 5000);
+            });
+    
+        // Set content hint to 'motion' or 'detail'
+        videoTrack.contentHint = 'motion';
+    }
+}
+
+async function waitToCompleteIceGathering(pc, logPerformance) {
+    const t0 = performance.now()
+  
+    let p = new Promise((resolve) => {
+      setTimeout(function () {
+        resolve(pc.localDescription)
+      }, 2500)
+      pc.onicegatheringstatechange = (ev) =>
+        pc.iceGatheringState === "complete" && resolve(pc.localDescription)
+    })
+  
+    return p
+  }
+
+function sendAnswerToBrowser(answerSDP) {
+    fetch("http://172.20.167.248:8000/answer", {
+        method: "post",
+        headers: {
+            'Content-Type': 'text/plain'
+        },
+        body: answerSDP
+    }).then(response=> response.json())
+}
